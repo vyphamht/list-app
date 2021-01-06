d17854d2c53d7f432d2675e3fde996d719cc0e38
phamhavy171 <59789678+phamhavy171@users.noreply.github.com>
2021-01-06 12:14:32 +0200

    fix buttons

--- src/components/MainMenu/MainMenu.js
+++ src/components/MainMenu/MainMenu.js
@@ -1,44 +1,38 @@
 import axios from "axios";
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 
 const MainMenu = () => {
   const [data, setData] = useState("");
   const [category, setCategory] = useState("");
 
-  const search = (e) => {
+  const buttonClicked = (e) => {
     e.preventDefault();
     setCategory(e.target.name);
-    console.log("clicked");
-    console.log(category);
-    if (category.length === 0) {
-      setData("loadingggggggg");
-    } else {
-      axios
-        .get(`/v2/products/${category}`, {
-          headers: {
-            "access-control-allow-origin": "*",
-            "Content-type": "application/json; charset=UTF-8",
-          },
-        })
-        .then((res) => {
-          console.log("button clicked");
-          const list = res.data.map((item) => (
-            <li key={item.id}>{item.name}</li>
-          ));
-          setData(list);
-        });
-    }
   };
+  useEffect(() => {
+    axios
+      .get(`/v2/products/${category}`, {
+        headers: {
+          "access-control-allow-origin": "*",
+          "Content-type": "application/json; charset=UTF-8",
+        },
+      })
+      .then((res) => {
+        console.log(`${category} is fired`);
+        const list = res.data.map((item) => <li key={item.id}>{item.name}</li>);
+        setData(list);
+      });
+  }, [category]);
 
   return (
     <div>
-      <button onClick={search} name="gloves">
+      <button onClick={buttonClicked} name="gloves">
         Gloves
       </button>
-      <button name="facemasks" onClick={search}>
+      <button name="facemasks" onClick={buttonClicked}>
         Face Masks
       </button>
-      <button name="beanies" onClick={search}>
+      <button name="beanies" onClick={buttonClicked}>
         Beanies
       </button>
       {data}
